fe1c8c44ea33f9892025c03411ed922021f25508
daswarudo <113220000+daswarudo@users.noreply.github.com>
2024-10-14 12:02:11 +0800

    newnew

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('login');
+    return view('welcome');//change to login to view login, welcome=dashboard
 });
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
