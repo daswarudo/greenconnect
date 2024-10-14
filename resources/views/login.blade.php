f72c0642b0d76230691901c993fb0db461b41a01
daswarudo <113220000+daswarudo@users.noreply.github.com>
2024-10-14 10:43:00 +0800

    added ui to repo

---
+++ resources/views/login.blade.php
@@ -0,0 +1,63 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+<!-- naa sa public/css folder ang css, ignore css sa resources-->
+    
+</head>
+<body>
+
+<script src="login.js"></script>
+<div class="container" id="container">
+	<div class="form-container sign-up-container">
+		<form action="#">
+			<h1>Create Account</h1>
+			
+			<span>or use your email for registration</span>
+			<input type="text" placeholder="Name" />
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<button>Sign Up</button>
+		</form>
+	</div>
+	<div class="form-container sign-in-container">
+		<form action="#">
+			<h1>Sign In</h1>
+			
+			<h3>Username</h3>
+			<input type="text" placeholder="Username" />
+            <h3>Password </h3>
+			<input type="password" placeholder="Password" />
+			<a href="#">Forgot your password?</a>
+			<button>Sign In</button>
+		</form>
+	</div>
+	<div class="overlay-container mobile-row">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h1>Welcome Back!</h1>
+				<p>To keep connected with us please login with your personal info</p>
+				<button class="ghost" id="signIn">Sign In</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h1>WELCOME TO GREEN CONNECT </h1>
+				<p>GREEN CHEF'S PORTAL TO NUTRITION </p>
+                <p>Don't have an account? </p>
+				<a class="ghost" id="signUp" href="SignUp.html">Sign Up</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Bootstrap JS and dependencies (optional) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
