1429f0de406844eed51c6f63dc4b746fb793650e
daswarudo <113220000+daswarudo@users.noreply.github.com>
2024-10-14 14:40:29 +0800

    comment

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,7 +9,7 @@
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
-<!-- naa sa public/css folder ang css, ignore css sa resources-->
+<!-- naa sa public/css folder ang css, ignore css sa resources sdfasfda-->
     
 </head>
 <body>
