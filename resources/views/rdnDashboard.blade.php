aa7a6d15e15979be8ed289a2c4983486b3b68d39
daswarudo <113220000+daswarudo@users.noreply.github.com>
2024-10-15 10:08:08 +0800

    rdn plus calendar

--- resources/views/rdnDashboard.blade.php
+++ resources/views/rdnDashboard.blade.php
@@ -4,10 +4,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form</title>
+    <title>GreenConnect Dashboard</title>
 
     
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">   ==>login css-->
+	<link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/main.min.css' rel='stylesheet' />
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/main.min.js'></script>
+    <script src='https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js'></script>
+
+	<script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                events: '/events',
+                selectable: true,
+                select: function(info) {
+                    var title = prompt('Event Title:');
+                    if (title) {
+                        axios.post('/events', {
+                            title: title,
+                            start: info.startStr,
+                            end: info.endStr
+                        }).then(response => {
+                            calendar.addEvent(response.data);
+                        });
+                    }
+                }
+            });
+            calendar.render();
+        });
+    </script>
+
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
     <link rel="stylesheet" href="{{ asset('css/rdnDash.css') }}">
 <!-- naa sa public/css folder ang css, ignore css sa resources sdfasfda-->
@@ -132,151 +160,10 @@
      <p>
       View your upcoming appointments
      </p>
-     <table>
-      <tr>
-       <th colspan="7">
-        September
-       </th>
-      </tr>
-      <tr>
-       <th>
-        SUN
-       </th>
-       <th>
-        MON
-       </th>
-       <th>
-        TUE
-       </th>
-       <th>
-        WED
-       </th>
-       <th>
-        THU
-       </th>
-       <th>
-        FRI
-       </th>
-       <th>
-        SAT
-       </th>
-      </tr>
-      <tr>
-       <td>
-        1
-       </td>
-       <td>
-        2
-       </td>
-       <td>
-        3
-       </td>
-       <td>
-        4
-       </td>
-       <td>
-        5
-       </td>
-       <td>
-        6
-       </td>
-       <td>
-        7
-       </td>
-      </tr>
-      <tr>
-       <td>
-        8
-       </td>
-       <td>
-        9
-       </td>
-       <td>
-        10
-       </td>
-       <td>
-        11
-       </td>
-       <td>
-        12
-       </td>
-       <td>
-        13
-       </td>
-       <td>
-        14
-       </td>
-      </tr>
-      <tr>
-       <td>
-        15
-       </td>
-       <td>
-        16
-       </td>
-       <td>
-        17
-       </td>
-       <td>
-        18
-       </td>
-       <td>
-        19
-       </td>
-       <td>
-        20
-       </td>
-       <td>
-        21
-       </td>
-      </tr>
-      <tr>
-       <td>
-        22
-       </td>
-       <td>
-        23
-       </td>
-       <td>
-        24
-       </td>
-       <td>
-        25
-       </td>
-       <td>
-        26
-       </td>
-       <td>
-        27
-       </td>
-       <td>
-        28
-       </td>
-      </tr>
-      <tr>
-       <td>
-        29
-       </td>
-       <td>
-        30
-       </td>
-       <td>
-        1
-       </td>
-       <td>
-        2
-       </td>
-       <td>
-        3
-       </td>
-       <td>
-        4
-       </td>
-       <td>
-        5
-       </td>
-      </tr>
-     </table>
+
+	<!--insert calendar-->
+	<div id='calendar'></div>
+		
     </div>
    </div>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
 
 Route::get('/', function () {
     return view('rdnDashboard');//change to login to view login, welcome=dashboard
@@ -21,3 +22,14 @@ Route::get('/signUp', function () {
 Route::get('/rdnDashboard', function () {
     return view('rdnDashboard');
 })->name('rdnDashboard');
+
+Route::get('/events', [EventController::class, 'index']);
+Route::post('/events', [EventController::class, 'store']);
+Route::get('/events/{id}', [EventController::class, 'show']);
+Route::put('/events/{id}', [EventController::class, 'update']);
+Route::delete('/events/{id}', [EventController::class, 'destroy']);
+
+
+
+
+
