364c27cdc0a917baff18f8042860c59df8d6cff0
daswarudo <113220000+daswarudo@users.noreply.github.com>
2024-11-03 18:17:20 +0800

    redo

---
+++ database/migrations/2024_11_03_101431_subscriptions.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id('subscription_id'); // Primary Key
+            $table->date('start_date');
+            $table->date('end_date')->nullable(); // Optional end date
+
+            //$table->foreignId('subscription_type_id')->constrained('subscription_types'); // Foreign Key
+            $table->unsignedBigInteger('subscription_type_id')->nullable();
+            $table->foreign('subscription_type_id')->references('subscription_type_id')->on('subscription_type')->onDelete('cascade');
+
+            $table->timestamps(); // created_at and updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
